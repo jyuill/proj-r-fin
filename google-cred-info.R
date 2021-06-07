7d58bd249edcc5702ad8a59e5957b1fea0d73867
John Yuill <john@fig4.com>
2021-06-06 19:04:26 -0700

    price and cost comparisons for krypto porto

---
+++ google-cred-info.R
@@ -0,0 +1,8 @@
+## google cred - needed for each computer
+
+## 1. Google Cloud Platform > get .json key file from Google Cloud Platform
+## 2. Download/save in repo folder
+## IMPORTANT !!! :
+## 3. ADD to .gitignore so it is NOT pushed up to GitHub repo 
+##    - EASY: Git > select file > gear > ignore (automatically adds to .gitignore)
+## (which is why you need to get new key file for each computer to store on hard drive only)

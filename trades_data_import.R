7305a00df83df3f08ea96f180071acf30f289c5d
John Yuill <john@fig4.com>
2021-07-24 15:57:00 -0700

    converting over to proper data from CB export. working through trade calculations. next is potential gains based on incorporating current price.

--- trades_data_import.R
+++ trades_data_import.R
@@ -9,24 +9,47 @@ library(googlesheets4)
 
 options(scipen=99)
 
-## DOWNLOAD DATA ####
+## STEPS: DOWNLOAD DATA ####
+## 1. CB > John > eDocs > activity report > download csv
+## 2. Go to google sheet (URL below)
+## 3. Check most recent transaction on sheet
+## 4. Copy/Paste new transactions from downloaded csv to sheet below existing
+
+## google auth - see 'google-cred-info.R' ####
+keyfile <- 'original-return-107905-3361ea00b21c.json' ## ?
+keyfile <- 'original-return-107905-8542c2ab0ad1.json' ## mac
+gs4_auth(path=here(keyfile),
+         scopes = "https://www.googleapis.com/auth/spreadsheets")
+
+## Get Google sheet ####
 ## Use google sheet URL to identify file
 sheet_id <- "https://docs.google.com/spreadsheets/d/1DRW5n6s6UtDAq3-kg-JTD4Yd2_24QfjAAxSU7-itkEE/edit?usp=sharing"
 
-keyfile <- 'original-return-107905-3361ea00b21c.json'
-gs4_auth(path=keyfile,
-         scopes = "https://www.googleapis.com/auth/spreadsheets")
-
 ## use read_sheet to call file
+## initial manual sheet - not used anymore but code maintained until system switched over 
 sheet_data <- read_sheet(sheet_id, sheet='Transactions - Form') ## skips first row, which has meta info in this case
 sheet_data <- sheet_data %>% filter(!is.na(Date))
 sheet_data$Date <- date(sheet_data$Date)
 
+## > CB EXPORT SHEET ####
+sheet_data_exp <- read_sheet(sheet_id, sheet='krypton-exp', skip=1) 
+sde <- sheet_data_exp
+sheet_data_exp <- sheet_data_exp %>% rename(
+  Date_time=Date
+) %>% mutate(
+  Date=date(Date_time)
+) %>% select(`Receipt No`, Date_time, Date, c(3:ncol(sheet_data_exp)))
+
 ## SAVE RAW DATA ####
+## original manual sheet
 write_csv(sheet_data, 'data/trades_data.csv')
 write_csv(sheet_data, paste0('data/trades_data',max(sheet_data$Date),'.csv'))
+## > CB exp sheet ####
+write_csv(sheet_data_exp, 'data/trades_data_exp.csv')
+write_csv(sheet_data_exp, paste0('data/trades_data_exp_',max(sheet_data_exp$Date),'.csv'))
 
-## CALCULATE COSTS ####
+## xCALC. COSTS [deprecated] ####
+## cost calculations made in new CB exp sheet in google sheets
 trade_data <- sheet_data %>% arrange(Symbol, Date)
 trade_data <- trade_data %>% group_by(Symbol) %>% mutate(
   units_chg=ifelse(Action=='Buy', Units, Units*-1),
@@ -49,6 +72,6 @@ trade_data <- trade_data %>% group_by(Symbol) %>% mutate(
   total_gain=ifelse(Action=='Sell', Total-(Units*acb),NA)
 )
 
-## SAVE ####
+## xSAVE [deprecated] ####
 write_csv(trade_data, 'data/trades_cost_data.csv')
 write_csv(trade_data, paste0('data/trades_cost_data',Sys.Date(),'.csv'))

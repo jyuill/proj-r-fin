af437ffc18c2f0154823654219a4bb871fa6ff04
John Yuill <john@fig4.com>
2025-05-19 14:44:21 -0700

    updates

--- google-cred-info.R
+++ google-cred-info.R
@@ -1,8 +1,21 @@
 ## google cred - needed for each computer
 
 ## 1. Google Cloud Platform > get .json key file from Google Cloud Platform
-## 2. Download/save in repo folder
+## 2. Create auth folder in project
+## 3. Download/save in auth/ folder
+## 4. Create .Renviron file at project root
+## 5. Add line to .Renviron file: GOOGLE_AUTH_JSON=auth/actual-file-name.json
+## 6. Restart RStudio to load .Renviron file
+## 7. Note: Add .Renviron to .gitignore so it is NOT pushed up to GitHub repo
+## 8. Test with: Sys.getenv('GOOGLE_AUTH_JSON')
+## 9. Use in code: (similar to below)
+#  if(Sys.getenv("GOOGLE_AUTH_JSON")!=""){
+#     gs4_auth(path = paste0("../",Sys.getenv("GOOGLE_AUTH_JSON")))
+#  } else {
+#.   stop("Please set up google auth json file")
+#  }
+
 ## IMPORTANT !!! :
-## 3. ADD to .gitignore so it is NOT pushed up to GitHub repo 
+## 10. ADD auth/ (and .Renviron) to .gitignore so it is NOT pushed up to GitHub repo 
 ##    - EASY: Git > select file > gear > ignore (automatically adds to .gitignore)
 ## (which is why you need to get new key file for each computer to store on hard drive only)
